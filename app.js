803c08502dd2475e6709eb63585cf57fb53ffdaf
StudioME - Left iMac <studiome-leftimac@StudioME-Left.local>
2020-01-16 17:17:57 -0500

    Adding Procfile

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+
 const cors = require('cors');
+
+// Set up port
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 
 const app = express();
 
@@ -18,7 +26,7 @@ mongoose.connect(`
     mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@studiome-server-1-1sjpe.gcp.mongodb.net/test?retryWrites=true&w=majority
 `, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
-    app.listen(4000);
+    app.listen(port);
     console.log('connected and listening on port 4000')
 })
 .catch(err => {
